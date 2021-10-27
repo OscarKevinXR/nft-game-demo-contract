159f1adf9073dd69001cdecaaf7ac040a09c03b3
Ousikai <mtfallsVR@protonmail.com>
2021-10-27 19:46:12 -0400

    add big boss params and attack logic

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,27 +7,43 @@ const main = async () => {
     "https://i.imgur.com/3Xjh2KF.jpg", 
     "https://i.imgur.com/hu6QwT4.jpg"],
     [200, 100, 300],                    // HP values
-    [50, 100, 25]                       // Attack damage values          
+    [50, 100, 25],                      // Attack damage values          
+    "Accelerator", // Boss name
+    "https://i.imgur.com/E3Y4V9P.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 
   
+  // let txn;
+  // txn = await gameContract.mintCharacterNFT(0);
+  // await txn.wait();
+  // console.log("Minted NFT #1");
+
+  // txn = await gameContract.mintCharacterNFT(1);
+  // await txn.wait();
+  // console.log("Minted NFT #2");
+
+  // txn = await gameContract.mintCharacterNFT(2);
+  // await txn.wait();
+  // console.log("Minted NFT #3");
+
+  // console.log("Done deploying and minting!");
+
+  // Test Boss Attack 
   let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
-
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
-  console.log("Minted NFT #3");
+  
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+  
+  txn = await gameContract.attackBoss();
+  await txn.wait();
 
-  console.log("Done deploying and minting!");
-
+  console.log("Done!");
 };
 
 const runMain = async () => {
--- scripts/run.js
+++ scripts/run.js
@@ -7,20 +7,35 @@ const main = async () => {
     "https://i.imgur.com/3Xjh2KF.jpg", 
     "https://i.imgur.com/hu6QwT4.jpg"],
     [200, 100, 300],                    // HP values
-    [50, 100, 25]                       // Attack damage values
+    [50, 100, 25],                      // Attack damage values
+    "Accelerator", // Boss name
+    "https://i.imgur.com/E3Y4V9P.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 
+  // let txn;
+  // // We only have three characters.
+  // // an NFT w/ the character at index 2 of our array.
+  // txn = await gameContract.mintCharacterNFT(2);
+  // await txn.wait();
+  
+  // // Get the value of the NFT's URI.
+  // let returnedTokenUri = await gameContract.tokenURI(1);
+  // console.log("Token URI:", returnedTokenUri);
+
+  // Test Boss Attack 
   let txn;
-  // We only have three characters.
-  // an NFT w/ the character at index 2 of our array.
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
   
-  // Get the value of the NFT's URI.
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log("Token URI:", returnedTokenUri);
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+  
+  txn = await gameContract.attackBoss();
+  await txn.wait();
 };
 
 const runMain = async () => {
